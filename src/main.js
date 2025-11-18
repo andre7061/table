8e84984851a770de7f8e414cd1312efbc6545729
Andrey Morozov <olgi82@yandex.ru>
2025-11-18 17:42:50 +0300

    init proect

---
+++ src/main.js
@@ -0,0 +1,39 @@
+import { data } from './data/data';
+import './style.css';
+
+const getIndexMap = (data, value, key) =>
+  data[value].reduce((acc, item) => {
+    acc[item[key]] = {
+      ...item,
+      fullName: `${item.first_name} ${item.last_name}`,
+    };
+    return acc;
+  }, {});
+
+const indexSellers = getIndexMap(data, 'sellers', 'id');
+const indexCustomers = getIndexMap(data, 'customers', 'id');
+
+const getCards = (data) =>
+  data.reduce((acc, card) => {
+    const customer = indexCustomers[card.customer_id];
+    const seller = indexSellers[card.seller_id];
+    if (customer && seller) {
+      acc.push({
+        ['date']: card.date,
+        ['customer']: customer.fullName,
+        ['seller']: seller.fullName,
+        ['total']: card.total_amount,
+      });
+    } else {
+      console.warn('Missing customer or seller for card:', card);
+    }
+
+    return acc;
+  }, []);
+
+const cards = getCards(data.purchase_records);
+
+console.log(indexSellers);
+console.log(indexCustomers);
+console.log(data);
+console.log(cards);
